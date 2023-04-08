13bacfcc23ec88c8a86fb772385775d3869d7c77
Tsanko Tsanev <tsanko.tsanev.business@gmail.com>
2023-04-08 22:23:39 +0300

    Initial commit

---
+++ routers/blog_post.py
@@ -0,0 +1,50 @@
+from fastapi import APIRouter, Body, Path, Query
+from pydantic import BaseModel
+from typing import Dict, List, Optional
+
+router = APIRouter(prefix="/blog", tags=["blog"])
+
+
+class Image(BaseModel):
+    url: str
+    alias: str
+
+
+class BlogModel(BaseModel):
+    title: str
+    content: str
+    num_comments: int
+    published: Optional[bool]
+    tags: List[str] = []
+    metadata: Dict[str, str] = {}
+    image: Optional[Image] = None
+
+
+@router.post("/new/{id}")
+def create_blog(blog: BlogModel, id: int, version: int = 1):
+    return {"id": id, "data": blog, "version": version}
+
+
+@router.post("/new/{id}/comment/{comment_id}")
+def create_comment(
+    blog: BlogModel,
+    id: int,
+    comment_title: str = Query(
+        None,
+        title="Title of the comment",
+        description="Some description for comment_title",
+        alias="commentTitle",
+        deprecated=True,
+    ),
+    content: str = Body(..., min_length=10, max_length=50, regex=r"^[a-z\s]*$"),
+    v: Optional[List[str]] = Query(["1.0", "1.1", "1.2"]),
+    comment_id: int = Path(gt=5, le=10),
+):
+    return {
+        "blog": blog,
+        "id": id,
+        "comment_title": comment_title,
+        "content": content,
+        "version": v,
+        "comment_id": comment_id,
+    }
