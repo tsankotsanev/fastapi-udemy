5fcd118fe070cb96e05506a49942692d35ddda17
Tsanko Tsanev <tsanko.tsanev.business@gmail.com>
2023-04-13 23:21:34 +0300

    Extend project

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI, Request, status
 from fastapi.responses import JSONResponse
 from exceptions import StoryException
-from routers import blog_get, blog_post, user, article, product
+from routers import blog_get, blog_post, user, article, product, file
 from auth import authentication
 from db import models
 from db.database import engine
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 app.include_router(authentication.router)
+app.include_router(file.router)
 app.include_router(user.router)
 app.include_router(article.router)
 app.include_router(product.router)
@@ -39,3 +41,5 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.mount("/files", StaticFiles(directory=("files")), name="files")
